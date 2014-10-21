998ec66efa9df41faf353694702f03a0c026e677
Huteri Manza <hut3ri@gmail.com>
2014-10-21 17:35:11 +0700

    add actionbar.tabs in home

--- app/src/main/java/com/mymonas/ngobrol/ui/ThreadFragment.java
+++ app/src/main/java/com/mymonas/ngobrol/ui/ThreadFragment.java
@@ -1,83 +1,71 @@
 package com.mymonas.ngobrol.ui;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.ProgressBar;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.mymonas.ngobrol.R;
-import com.mymonas.ngobrol.io.RestClient;
-import com.mymonas.ngobrol.io.model.ThreadCallback;
-import com.mymonas.ngobrol.model.ThreadItem;
-import com.mymonas.ngobrol.ui.adapter.ThreadAdapter;
-import com.mymonas.ngobrol.util.Clog;
 
-import java.util.ArrayList;
-
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
-
-public class ThreadFragment extends Fragment {
-    private static final String ACTION_GET_ALL_THREADS = "get_all_threads";
-    private Context mContext;
-    private ArrayList<ThreadItem> mThreadList;
-    private ThreadAdapter mThreadAdapter;
+/**
+ * Created by Huteri on 10/21/2014.
+ */
+public class ThreadFragment extends Fragment{
+    private ViewPager mViewPager;
+    private ThreadPagerAdapter mPagerAdapter;
 
     public ThreadFragment() {
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        mContext = getActivity();
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_thread, container, false);
 
-        mThreadList = new ArrayList<ThreadItem>();
-        mThreadAdapter = new ThreadAdapter(mContext, mThreadList);
+        mPagerAdapter = new ThreadPagerAdapter(getActivity().getSupportFragmentManager());
+        mViewPager = (ViewPager) view.findViewById(R.id.pager);
+        mViewPager.setAdapter(mPagerAdapter);
 
-        final ProgressBar pBar = (ProgressBar) view.findViewById(R.id.progress_bar);
-        pBar.setVisibility(View.VISIBLE);
-        RestClient.get().getThreads(ACTION_GET_ALL_THREADS, new Callback<ThreadCallback>() {
-            @Override
-            public void success(ThreadCallback threadCallback, Response response) {
-                pBar.setVisibility(View.GONE);
-                mThreadList.addAll(threadCallback.getData());
-                mThreadAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                pBar.setVisibility(View.GONE);
-                Clog.d(error.getCause().toString());
-            }
-        });
-
-        ListView threadLv = (ListView) view.findViewById(R.id.thread_list);
-        View header = ((LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.thread_header, null);
-
-        threadLv.addHeaderView(header);
-        threadLv.setAdapter(mThreadAdapter);
-        threadLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Bundle args = new Bundle();
-                args.putString("threadId", mThreadList.get(i-1).getId());
-
-                Intent intent = new Intent(getActivity(), PostActivity.class);
-                intent.putExtras(args);
-                startActivity(intent);
-            }
-        });
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
+        tabs.setDividerColor(getResources().getColor(R.color.divider));
+        tabs.setTextColor(getResources().getColor(R.color.theme_color));
+        tabs.setIndicatorColor(getResources().getColor(R.color.theme_color));
+        tabs.setShouldExpand(true);
+        tabs.setViewPager(mViewPager);
 
         return view;
     }
+
+    public class ThreadPagerAdapter extends FragmentStatePagerAdapter {
+
+        String[] titles = {"Categories", "Hot Threads", "New Threads"};
+
+        public ThreadPagerAdapter(FragmentManager fm) {
+            super(fm);
+
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return Fragment.instantiate(getActivity(), HotThreadFragment.class.getName());
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+    }
+
+
+
 }
