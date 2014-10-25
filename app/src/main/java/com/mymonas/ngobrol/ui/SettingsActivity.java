def81122c71ad6ef3ae23b985498b404ca8f52ac
Huteri Manza <hut3ri@gmail.com>
2014-10-25 21:15:01 +0700

    add (about) about the app and team

--- app/src/main/java/com/mymonas/ngobrol/ui/SettingsActivity.java
+++ app/src/main/java/com/mymonas/ngobrol/ui/SettingsActivity.java
@@ -1,10 +1,13 @@
 package com.mymonas.ngobrol.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.view.MenuItem;
 
 import com.mymonas.ngobrol.R;
+import com.mymonas.ngobrol.util.AboutActivity;
 
 public class SettingsActivity extends PreferenceActivity {
 
@@ -15,6 +18,15 @@ public class SettingsActivity extends PreferenceActivity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
         addPreferencesFromResource(R.xml.pref_general);
 
+        Preference about = (Preference) findPreference("pref_about");
+        about.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(SettingsActivity.this, AboutActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/mymonas/ngobrol/util/AboutActivity.java
@@ -0,0 +1,33 @@
+package com.mymonas.ngobrol.util;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.mymonas.ngobrol.R;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
