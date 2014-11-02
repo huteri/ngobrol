6519916760dd15e3025202f68c10e13fa698b0cc
Huteri Manza <hut3ri@gmail.com>
2014-11-02 23:20:19 +0700

    fix (search menu) remove for a while

--- app/src/main/java/com/mymonas/ngobrol/ui/CategoryThreadActivity.java
+++ app/src/main/java/com/mymonas/ngobrol/ui/CategoryThreadActivity.java
@@ -1,13 +1,16 @@
 package com.mymonas.ngobrol.ui;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.mymonas.ngobrol.R;
 import com.mymonas.ngobrol.model.CategoryItem;
+import com.mymonas.ngobrol.util.UserUtils;
 
 /**
  * Created by Huteri on 10/30/2014.
@@ -39,10 +42,26 @@ public class CategoryThreadActivity extends FragmentActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        UserUtils userUtils = new UserUtils(this);
+        if(!userUtils.isAvailable()) {
+            MenuItem item = menu.findItem(R.id.action_new);
+            item.setVisible(false);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
             case android.R.id.home:
                 finish();
+                break;
+            case R.id.action_new:
+                Intent intent = new Intent(this, AddEditThreadActivity.class);
+                startActivity(intent);
                 break;
         }
         return super.onOptionsItemSelected(item);
